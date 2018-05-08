41c3ebc53dcdf9fe8d2f7b49449d3a58c7e4dc6e
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-05-08 11:54:43 +0530

    Initial commit

---
+++ sorting.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	int *a,i,j,tmp,n,p[100];
+	cout<<"Enter the no. of elements of the array :";
+	cin>>n;
+	cout<<"\nEnter the elements of the array :";
+	for(i=0;i<n;i++){
+		cin>>p[i];	
+	}
+	a=p;
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(*(a+i)>*(a+j)){
+				tmp=*(a+i);
+				*(a+i)=*(a+j);
+				*(a+j)=tmp;
+			}
+		}
+	}
+	cout<<"\nElements after sorting : \n";
+	for(i=0;i<n;i++)
+	cout<<*(a+i)<<" ";
+}
