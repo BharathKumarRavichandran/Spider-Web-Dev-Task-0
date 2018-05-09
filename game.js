4ecf999e25fa6dbc083eb226e95f3bdddf41c93f
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-05-09 20:04:32 +0530

    Bug fixed. Time, speed displays added

--- game.js
+++ game.js
@@ -1,29 +1,41 @@
+var counter;
 var date = new Date();
 var d1;
 var d2;
+var i=0;
+var len=0;//Paragraph's length by letter
+var n;//No. of words in the paragraph
 var t=0;
-var width = window.innerWidth;
-var height = window.innerHeight;
-var w = width/2;
+var t1;
+var t2;
+var input = document.getElementById("input");
 var stButton = document.getElementById("start");
 var speedDisplay = document.getElementById("speedDisplay");
 var speedDisplaySpan = document.getElementById("speedDisplaySpan");
+var text = new Array();
+var test = document.getElementById("test");
 var timeDisplay = document.getElementById("timeDisplay");
 var timeDisplaySpan = document.getElementById("timeDisplaySpan");
 var timerText = document.getElementById("timerText");
+var width = window.innerWidth;
+var height = window.innerHeight;
+var w = width/2;
+
 stButton.style.marginLeft = (w-40)+'px';
 speedDisplay.style.marginLeft = (w-130)+'px';
 timeDisplay.style.marginLeft = (w-130)+'px';
-var i=0;
-var n=1;
-var acc=true;
-var input = document.getElementById("input");
-var text = new Array();
-text = document.getElementById("para").innerHTML;
 
+function wordCount(str) { //Function to count the no. of words in the paragraph.
+  return str.split(" ").length;
+}
 
 function startTimer(){
-  var counter = 4; 
+  
+  text = document.getElementById("para").innerHTML;
+  counter = 4;
+  len = text.length; 
+  n= wordCount(text);
+  
   setInterval(function() {
     counter--;
     if (counter >= 1) {
@@ -41,7 +53,6 @@ function startTimer(){
     	clearInterval(counter);
     	return;
     }
-    timeDisplaySpan.innerHTML = date.getTime()-d1;
   }, 1000);
 }
 /*
@@ -54,7 +65,7 @@ function wordCheck(){
 				}
 			}, false);
 }*/
-
+/*
 function wordCheck(){
           //alert(text[i]+"  "+input.value);    
           if(input.value == text[i]){
@@ -67,5 +78,35 @@ function wordCheck(){
             input.value="";
             acc = false;
 
+          }
+}*/      
+function wordCheck(){ 
+          if(i==0){
+            d1 = new Date();
+            t1 = d1.getTime();
+          }
+
+          d2 = new Date();
+          t2 = d2.getTime();
+          t=t2-t1;// Time taken in milliseconds
+          timeDisplaySpan.innerHTML = t/1000+" seconds";
+          
+          if(input.value == text[i]){
+            i++;
+            input.value="";
+            test.style.backgroundColor = "green";
+          }
+          else{
+            test.style.backgroundColor = "red";
+            input.value="";
+          }
+          if(i==len-1){
+            d2 = new Date();
+            t2 = d2.getTime();
+            t=t2-t1;// Time taken in milliseconds
+            speedDisplaySpan.innerHTML = Math.round(n*1000*60/t)+" wpm";
+            test.style.backgroundColor = "blue";
+            input.disabled=true;
+            input.value="Successfully completed!";
           }
 }      
